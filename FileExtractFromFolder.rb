ceac1b6e8e573cecc6009e6c419f522a6813f333
aditya pradhan <pradhan.aditya@gmail.com>
2012-06-21 11:48:44 -0400

    Generalized the folder names for reusability

--- FileExtractFromFolder.rb
+++ FileExtractFromFolder.rb
@@ -1,10 +1,15 @@
-# This is a script to extract all the MT zip files from the folder
+# This is a script to extract all the zip files from the folder
 # @adi
 # kind of ridiculous how this script is just 10 lines including the comments.
 require 'FileUtils'
 
-Dir.chdir('D:\Seeburger\Messages\ANSI_X12\\ZIPS')
+puts 'What is the Folder containing the zip Files' 
+ZipsFolder = gets.chomp
+
+puts 'What is the folder to copy to ' 
+CopyTo = gets.chomp
+Dir.chdir(ZipsFolder)
 ZipFiles = Dir.glob('**/*.zip')
 ZipFiles.each do |zip|
-FileUtils.copy_file(zip , 'D:\\Seeburger\\Messages\\' + zip.split('/').last)
+FileUtils.copy_file(zip , CopyTo+ zip.split('/').last)
 end 
